a672a4159acce3dcd0dd4994e93ee074db0b53b2
JiHwan Choi <jhcplace@daum.net>
2022-01-21 15:36:11 +0900

    Add files via upload

--- elements/menu.tsx
+++ elements/menu.tsx
@@ -1,7 +1,6 @@
 import menu_styles from '../styles/Menu.module.css';
 import classnames from 'classnames';
 import Link from 'next/link';
-import wait from "waait";
 
 // import react things
 import { useState } from 'react';
@@ -40,4 +39,40 @@ export default function Menu() {
             </div>
         </>
     );
+}
+
+function DesktopMenu() {
+    return <>
+        <div className={menu_styles.menu}>
+            <Link passHref href="/"><div className={menu_styles.menu_title}>PLUG</div></Link>
+            <div className={menu_styles.flex}>
+                <Link passHref href="/info"><div className={menu_styles.menu_btn}>소개</div></Link>
+                <Link passHref href="/contact"><div className={menu_styles.menu_btn}>연락</div></Link>
+            </div>
+        </div>
+    </>
+}
+function MobileMenu() {
+    let [isMobileMenuOpened, SetMobileIcon] = useState(false);
+
+    async function MobileMenu() {
+        isMobileMenuOpened = !isMobileMenuOpened;
+        SetMobileIcon(isMobileMenuOpened);
+    }
+
+    return <>
+        <div style={ isMobileMenuOpened === true ? { backgroundColor: 'white' } : {}} className={menu_styles.mobile_menu}>
+            <Link passHref href="/"><div className={menu_styles.menu_title}>PLUG</div></Link>
+            <div>{ isMobileMenuOpened === false ? <div onClick={() => {MobileMenu()}} className={classnames('material-icons', menu_styles.menu_icon)}>menu</div> : <div onClick={() => {MobileMenu()}} className={classnames('material-icons', menu_styles.menu_icon)}>close</div> }</div>
+            { isMobileMenuOpened === false ?  
+                <div className={classnames(menu_styles.flex, menu_styles.mobile_menu_content, menu_styles.mobile_menu_closed)}>
+                    <Link passHref href="/info"><div style={{ display: 'none' }} className={menu_styles.menu_btn}>소개</div></Link>
+                    <Link passHref href="/contact"><div style={{ display: 'none' }} className={menu_styles.menu_btn}>연락</div></Link>
+                </div> :  
+                <div className={classnames(menu_styles.flex, menu_styles.mobile_menu_content)}>
+                    <Link passHref href="/info"><div style={{ display: 'block' }} className={menu_styles.menu_btn}>소개</div></Link>
+                    <Link passHref href="/contact"><div style={{ display: 'block' }} className={menu_styles.menu_btn}>연락</div></Link>
+            </div> }
+        </div>
+    </>
 }
